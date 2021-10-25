504cc0cbaceb4061c951dddf1a74daec525b936e
PushpanjaliSonawane <pushpasonavane96@gmail.com>
2021-10-25 14:40:15 +0530

    initial commit

---
+++ Java_S1.js
@@ -0,0 +1,14 @@
+function myFunction()
+{
+	alert("Hello!I am alert box!");
+}
+function calculator()
+{
+	var a=10;
+	var b=20;
+	a+b;
+	a-b;
+	a*b;
+	a/b;
+}
+document.getElementById("try").onclick=myFunction;
